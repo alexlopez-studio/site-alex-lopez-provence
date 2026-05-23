c045c2d3d7fbe96c9b8b58f19a15ce08b0123228
alexlopez <alexlopez.studio@gmail.com>
2026-05-24 01:29:06 +0200

    fix(analytics): move GTM script into body layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -61,14 +61,14 @@ export default async function RootLayout({
 
   return (
     <html lang={htmlLang} className={inter.variable + ' ' + buffalo.variable}>
-      <Script id="google-tag-manager" strategy="afterInteractive">
-        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      <body className="font-sans antialiased">
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','${GTM_ID}');`}
-      </Script>
-      <body className="font-sans antialiased">
+        </Script>
         <noscript>
           <iframe
             src={'https://www.googletagmanager.com/ns.html?id=' + GTM_ID}
