009e77457b025a2944eabb28ab47c55beb906e78
alexlopez <alexlopez.studio@gmail.com>
2026-05-24 01:25:42 +0200

    feat(analytics): add Google Tag Manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,6 +48,8 @@ export const metadata: Metadata = {
   robots: { index: true, follow: true },
 }
 
+const GTM_ID = 'GTM-T3P59HCW'
+
 export default async function RootLayout({
   children,
 }: {
@@ -59,7 +61,23 @@ export default async function RootLayout({
 
   return (
     <html lang={htmlLang} className={inter.variable + ' ' + buffalo.variable}>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GTM_ID}');`}
+      </Script>
       <body className="font-sans antialiased">
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style= display: 'none', visibility: 'hidden' 
+            title="Google Tag Manager"
+          />
+        </noscript>
         <AnalyticsScripts />
         <NextIntlClientProvider locale={locale} messages={messages}>
           <LinkClickTracker />
